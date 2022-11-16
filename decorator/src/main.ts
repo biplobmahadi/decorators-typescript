9e734057258267b69541b351a38186a3734bdfe1
Biplob <biplob.ruet15@gmail.com>
2022-11-16 12:16:43 +0600

    create a decorator factory to get exra useful power

--- decorator/src/main.ts
+++ decorator/src/main.ts
@@ -1,8 +1,9 @@
-const Logger = (constructor: Function) => {
-  console.log('logger deco');
+const Logger = (logStr: string) => (constructor: Function) => {
+  console.log(logStr);
   console.log(constructor);
 };
-@Logger
+
+@Logger('I can put custom logger msg')
 class Person {
   private name: string;
   constructor(name: string) {
