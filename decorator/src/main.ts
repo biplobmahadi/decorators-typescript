2fc700905f46a4321ea648bd6d2796d3164f4e0c
Biplob <biplob.ruet15@gmail.com>
2022-11-16 13:11:01 +0600

    add property, method, accessor, param decorator. and also see the execution of all decorators

--- decorator/src/main.ts
+++ decorator/src/main.ts
@@ -1,28 +1,70 @@
 const Logger = (logStr: string) => {
   console.log('Logger 1 Factory');
-  return (constructor: Function) => {
+  return (_constructor: Function) => {
     console.log(logStr);
-    console.log(constructor);
   };
 };
 const Logger2 = (logStr: string) => {
   console.log('Logger 2 Factory');
-  return (constructor: Function) => {
+  return (_constructor: Function) => {
     console.log(logStr);
-    console.log(constructor);
   };
 };
 
-@Logger('I can put custom logger msg')
-@Logger2('logger 2')
+const PropertyDeco = (target: any, propertyName: string) => {
+  console.log(target, propertyName);
+};
+
+const AccessorGetDeco = (
+  target: any,
+  name: string,
+  descriptor: PropertyDescriptor
+) => {
+  console.log(target, name, descriptor);
+};
+const AccessorSetDeco = (
+  target: any,
+  name: string,
+  descriptor: PropertyDescriptor
+) => {
+  console.log(target, name, descriptor);
+};
+
+const MethodDeco = (
+  target: any,
+  name: string,
+  descriptor: PropertyDescriptor
+) => {
+  console.log(target, name, descriptor);
+};
+const ParamDeco = (target: any, name: string, positon: number) => {
+  console.log(target, name, positon);
+};
+
+@Logger('logger 1 deco')
+@Logger2('logger 2 deco')
 class Person {
+  @PropertyDeco
   private name: string;
-  constructor(name: string) {
+  private age: number;
+
+  constructor(name: string, age: number) {
     this.name = name;
+    this.age = age;
   }
 
-  showName() {
-    console.log(this.name, 'is a good boy.');
+  @AccessorSetDeco
+  set ageAdd(@ParamDeco age: number) {
+    this.age = age;
+  }
+  @AccessorGetDeco
+  get ageGet() {
+    return this.age;
+  }
+
+  @MethodDeco
+  showName(@ParamDeco msg: string) {
+    console.log(this.name, 'is a good boy. And age is: ', this.age, msg);
   }
 }
 
