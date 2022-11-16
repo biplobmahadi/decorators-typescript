877bd34915c0ec9a47f9ded8d072728e823c6c09
Biplob <biplob.ruet15@gmail.com>
2022-11-16 12:26:32 +0600

    bottom up decorators run when multiple applied, also factory run top to bottom first

--- decorator/src/main.ts
+++ decorator/src/main.ts
@@ -1,9 +1,20 @@
-const Logger = (logStr: string) => (constructor: Function) => {
-  console.log(logStr);
-  console.log(constructor);
+const Logger = (logStr: string) => {
+  console.log('Logger 1 Factory');
+  return (constructor: Function) => {
+    console.log(logStr);
+    console.log(constructor);
+  };
+};
+const Logger2 = (logStr: string) => {
+  console.log('Logger 2 Factory');
+  return (constructor: Function) => {
+    console.log(logStr);
+    console.log(constructor);
+  };
 };
 
 @Logger('I can put custom logger msg')
+@Logger2('logger 2')
 class Person {
   private name: string;
   constructor(name: string) {
